46b0fdfcc7260ff15a3b27a49294afe74f0c2f52
ashleydivine <ashleynneji@gmail.com>
2015-02-24 18:47:12 -0600

    New Course.java
    Replaced Alex course file with my code

--- classes/Course.java
+++ classes/Course.java
@@ -1,7 +1,45 @@
 public class Course
 {
-	public void setCourseNo()
-	{
-		
-	}
+  private String courseName;
+  private String courseNo;
+  private double credits;
+ 
+  
+  
+  public void setCourseNo(String cno)
+  {
+    this.courseNo= cno;
+  }
+  
+  public String getCourseNo()
+  {
+    return courseNo;
+  }
+  
+  public void setCourseName(String name)
+  {
+    this.courseName= name;
+    
+  }
+  public String getCourseName()
+  {
+    return courseName;
+  }
+  public void setCredits(double c)
+  {
+    this.credits= c;
+  }
+  public double getCredits()
+  {
+    return credits;
+  }
+  public String toString()
+  {
+    return courseNo;
+  }
 }
+  
+
+    
+  
+  
