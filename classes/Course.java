4c48888ae056850cfb68668a8796338682b67fd9
AlexSeefeldt <xseefeldt@gmail.com>
2015-02-24 19:06:01 -0600

    Edited and compiled new Course
    I edited the Course.java file a bit for consistency and then compiled
    it.

--- classes/Course.java
+++ classes/Course.java
@@ -3,40 +3,48 @@ public class Course
   private String courseName;
   private String courseNo;
   private double credits;
- 
   
-  
-  public void setCourseNo(String cno)
+  public Course(String courseName, String courseNo)
   {
-    this.courseNo= cno;
+  	this.courseName = courseName;
+  	this.courseNo = courseNo;
+  }
+  
+  public void setCourseName(String courseName)
+  {
+    this.courseName = courseName;
+  }
+
+  public String getCourseName()
+  {
+    return courseName;
+  }
+
+  public void setCourseNo(String courseNo)
+  {
+    this.courseNo = courseNo;
   }
   
   public String getCourseNo()
   {
     return courseNo;
   }
-  
-  public void setCourseName(String name)
-  {
-    this.courseName= name;
-    
-  }
-  public String getCourseName()
-  {
-    return courseName;
-  }
+
   public void setCredits(double c)
   {
-    this.credits= c;
+    this.credits = c;
   }
+
   public double getCredits()
   {
     return credits;
   }
+
   public String toString()
   {
     return courseNo;
   }
+
 }
   
 
