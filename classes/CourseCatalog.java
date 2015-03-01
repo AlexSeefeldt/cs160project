3247d7eb6790523301d7ebe138cc34022e768a08
erinqueme <erinquemeeastend@gmail.com>
2015-03-01 17:37:48 -0600

    CourseCatalog.java
    CourseCatalog

---
+++ classes/CourseCatalog.java
@@ -0,0 +1,25 @@
+public class CourseCatalog extends Course
+{
+	private Map<courseNo,Course> courses;
+
+	public void display()
+	{
+		//what goes here?
+	}
+
+	public void addCourse(Course)
+	{
+		this.Course = Course;
+	}
+
+	public Course findCourse(courseNo)
+	{
+		this.courseNo = courseNo;
+		getCourseNo(courseNo);
+	}
+	public Boolean isEmpty()
+	{
+		if(courseNo == 0);
+			return "This class is empty."
+	}
+}
