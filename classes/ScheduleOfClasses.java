b32761f3b3741ad5d53f591f152d788bfa99cc04
ashleydivine <ashleynneji@gmail.com>
2015-03-02 14:51:43 -0600

    ScheduleOfClasses.java
    Finished the methods we have covered in class . Professor Rasanjalee
    said I shouldn't bother with the hash map methods because we haven't
    covered those in class

---
+++ classes/ScheduleOfClasses.java
@@ -0,0 +1,33 @@
+
+/**
+ * this class models the schedule of classes
+ */
+public class ScheduleOfClasses
+{
+	private String semester;
+/**
+ * The constructor initializes the string variable
+ * @param semester
+ */
+	public ScheduleOfClasses(semester)
+	{
+       this.semester= semester;
+	}
+  /**
+   * sets the semester 
+   * @param semester
+   */
+	public void setSemester(semester)
+	{
+		this.semester= semester;
+    }
+    /**
+     * gets the specific semester
+     * @return semester
+     */
+    public String getSemester()
+    {
+    	return semester;
+    }
+
+}
