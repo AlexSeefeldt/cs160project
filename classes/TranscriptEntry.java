910b8b9cfa4e3c11c11eda15b13edd5908b8b8c5
AlexSeefeldt <xseefeldt@gmail.com>
2015-03-12 21:34:43 -0500

    Edits and compile for TranscriptEntry
    I moved TranscriptEntry to the right folder, gave it the filetype .java,
    made a few edits, and compiled it.

--- classes/TranscriptEntry.java
+++ classes/TranscriptEntry.java
@@ -1,9 +1,72 @@
 public class TranscriptEntry
 {
-	private Section section;
-
-	public Section getSection()
-	{
-		return this.section;
-	}
+  private String grade;
+  private Student student;
+  private Section section;
+  private Transcript transcript;
+ 
+  public TranscriptEntry(Student student, Section section, Transcript transcript)
+  {
+    this.student = student;
+    this.section = section;
+    this.transcript = transcript;
+  }
+ 
+  public void setStudent(Student student)
+  {
+    this.student = student;
+  }
+ 
+  public void setSection(Section section)
+  {
+    this.section = section;
+  }
+ 
+  public void setGrade(String grade)
+  {
+    this.grade = grade;
+  }
+  
+  public void setTranscript(Transcript transcript)
+  {
+    this.transcript = transcript;
+  }
+  
+  public Student getStudent()
+  {
+    return this.student;
+  }
+ 
+  public Section getSection()
+  {
+    return this.section;
+  }
+  
+  public String getGrade()
+  {
+    return this.grade;
+  } 
+  
+  public Transcript getTranscript()
+  {
+    return this.transcript;
+  }
+  
+  public Boolean validateGrade(String grade)
+  {
+    if(grade == null) //Do not understand what to compare grade to.
+    {
+      return true;
+    }
+    return false;
+  }
+  
+  public Boolean passingGrade(String grade)
+  {
+    if(grade == "A" || grade == "B" || grade == "C" || grade == "D" )
+    {
+      return true;
+    }
+    return false;
+  }
 }
