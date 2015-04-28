964f40ff281d6e649ae9d068940b1e1b7a202e9f
ashleydivine <ashleynneji@gmail.com>
2015-04-28 14:31:48 -0500

    Main

--- classes/MainFrame.java
+++ classes/MainFrame.java
@@ -1,33 +1,94 @@
-import java.awt.event.*;
-import java.awt.*;
-import java.util.*;
-import javax.swing.*;
-import javax.swing.event,*;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.util.Random;
+import javax.swing.JFrame;
+import javax.swing.JDesktopPane;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import javax.swing.JInternalFrame;
 
 public class MainFrame extends JFrame
 {
-  public class TheMainFrame
-{
-  private JPanel upLeftPanel;
-  private JPanel downLeftPanel;
-  private JPanel labPanel;
-  private JPanel leftPanel;
-  private JList scheduleOfClasses;
-  private JTextField ssn;
-  private JButton add;
-  private JButton logOff;
-  private JButton saveSchedule;
-  private JButton drop;
-  private JLabel name;
-  private JLabel totalCourses;
+  private JDesktopPane theMainFrame;
   
-  
-  
-  public TheMainFrame()
+  public MainFrame()
   {
-    this.setTitle("Student Registration System");
-    this.setSize(600,400);
-
-
+    super( "Using a JDesktopPane" );
+    JMenuBar bar = new JMenuBar(); // create menu bar
+    JMenu fileMenu = new JMenu( "File" ); // create Add menu
+    JMenu studentMenu= new JMenu("Student");
+    JMenu professorMenu= new JMenu("Professor");
+    JMenuItem fileFrameItem = new JMenuItem( "Login" );
+    JMenuItem fileFrameItem2= new JMenuItem("LogOff");
+    JMenuItem fileFrameItem3= new JMenuItem("Close"); 
+    JMenuItem studentFrameItem= new JMenuItem("Display/Find Student");
+    JMenuItem studentFrameItem2= new JMenuItem("Display Course Schedule");
+    JMenuItem studentFrameItem3= new JMenuItem("Add Section");
+    JMenuItem studentFrameItem4= new JMenuItem("Drop Section");
+    JMenuItem studentFrameItem5= new JMenuItem("View Transcript");
+    JMenuItem professorFrame= new JMenuItem("Display/Find Professor");
+    JMenuItem professorFrame2= new JMenuItem("Displaying Teaching Assignments");
+    JMenuItem professorFrame3= new JMenuItem("Display Student Roster");
+    JMenuItem professorFrame4= new JMenuItem("Agree to teach a course");                                        
+    fileMenu.add(fileFrameItem); // add new frame item to Add menu
+    fileMenu.add(fileFrameItem2);
+    fileMenu.add(fileFrameItem3); 
+    studentMenu.add(studentFrameItem);
+    studentMenu.add(studentFrameItem2);
+    studentMenu.add(studentFrameItem3);
+    studentMenu.add(studentFrameItem4);
+    studentMenu.add(studentFrameItem5);
+    professorMenu.add(professorFrame);
+    professorMenu.add(professorFrame2); 
+    professorMenu.add(professorFrame3);
+    professorMenu.add(professorFrame4);
+    bar.add(fileMenu ); // add Add menu to menu bar
+    setJMenuBar( bar ); // set menu bar for this application
+    bar.add(studentMenu);
+    setJMenuBar(bar);
+    bar.add(professorMenu);
+    setJMenuBar(bar);
+    theMainFrame = new JDesktopPane(); // create desktop pane
+    add( theMainFrame ); // add desktop pane to frame
+    
+    // set up listener for newFrame menu item
+    ActionListener newFrameMenuItemlistener = new NewFrameMenuItemListner();
+    fileFrameItem.addActionListener(newFrameMenuItemlistener);
+    fileFrameItem2.addActionListener(newFrameMenuItemlistener);
+    fileFrameItem3.addActionListener(newFrameMenuItemlistener);
+    studentFrameItem.addActionListener(newFrameMenuItemlistener);
+    studentFrameItem2.addActionListener(newFrameMenuItemlistener);
+    studentFrameItem3.addActionListener(newFrameMenuItemlistener);
+    studentFrameItem4.addActionListener(newFrameMenuItemlistener);
+    studentFrameItem5.addActionListener(newFrameMenuItemlistener);
+    professorFrame.addActionListener(newFrameMenuItemlistener);
+    professorFrame2.addActionListener(newFrameMenuItemlistener);
+    professorFrame3.addActionListener(newFrameMenuItemlistener);
+    professorFrame4.addActionListener(newFrameMenuItemlistener);
+    
+  } 
+class NewFrameMenuItemListner implements ActionListener
+{  
+  public void actionPerformed(ActionEvent event)
+  {  
+    JInternalFrame frame = new CheckBoxInternalFrame( "", true, true, true, true );     
+    theMainFrame.add( frame ); // attach internal frame
+    frame.setVisible( true ); // show internal frame
+    frame.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+    
+  }
 }
-	}
+public static void main( String[] args )
+{ 
+  MainFrame theMainFrame = new MainFrame();  
+  theMainFrame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+  theMainFrame.setSize( 600, 480 ); // set frame size
+  theMainFrame.setVisible( true ); // display frame
+}
+}
+
+  
